eb370ffb3c35851ffa2486150383751535e2447e
Cristian Torrico <cristian.torrico@hotmail.com.ar>
2024-02-17 12:33:36 -0300

    Add category filter to product list

--- src/layouts/product/PageListProduct.jsx
+++ src/layouts/product/PageListProduct.jsx
@@ -3,25 +3,61 @@ import {useState,useEffect} from 'react'
 import { Product } from '../../components/Product';
 import Title from '../../components/Title';
 import '../../assets/styles/pages/PageListProduct.scss';
-import { getDocs, collection, getFirestore } from 'firebase/firestore';
+import { getDocs, collection, getFirestore, query, where } from 'firebase/firestore';
 
 
 const PageListProduct = () => {
   const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState('todos');
 
+//   useEffect(()=>{
+
+//     const db = getFirestore() //instanciamos
+
+//     const productsCollection = collection(db, 'items') //declaramos collection
+
+//     getDocs(productsCollection)
+//         .then( snapshot => {
+//             const dataExtraida = snapshot.docs.map(datos => datos.data())
+//             setProducts(dataExtraida);
+//         })                
+
+//   },[]);
+
+
+
+  //Nosotros necesitamos, que al cambiar el valor de la categoria, tambien se actulice los productos
   useEffect(()=>{
 
-    const db = getFirestore() //instanciamos
+    //Necesitamos llamar a firestore y traer todas las gorras 
+    const db = getFirestore();
+    
 
-    const productsCollection = collection(db, 'items') //declaramos collection
+    //Devolver todos o solo devolver el valor de la categoria
+    if(category == 'todos'){
 
-    getDocs(productsCollection)
+        var q = query(
+            collection(db, 'items')
+        )
+
+    }else{
+
+        var q = query(
+            collection(db, 'items'),
+            where('categoria', '==', category)
+        )
+
+    }
+
+    getDocs(q)
         .then( snapshot => {
             const dataExtraida = snapshot.docs.map(datos => datos.data())
             setProducts(dataExtraida);
-        })                
+        })
 
-  },[]);
+
+
+  },[category])
 
 
   return (
@@ -31,6 +67,28 @@ const PageListProduct = () => {
                   <h2>
                       <Title title="Listado de productos" type="primary"/>         
                   </h2>
+
+                  <div className="row">
+                    <select name="categories" id="categories" value={category} onChange={
+                        (event) => {
+                            setCategory(event.target.value) // <-- setCategory('gorras')
+                        }
+                    } 
+                    
+                    
+                        style={
+                            {
+                                marginBottom: '40px',
+                                height: '30px'
+                            }
+                        }
+                    >
+                        <option value="todos">Todos</option>
+                        <option value="zapatillas">Zapatillas</option>
+                        <option value="gorras">Gorras</option>
+                    </select>
+                  </div>
+
                   <div className="row">
                       {
                           products.map(product => <Product key={product.id} product={product}></Product>)
